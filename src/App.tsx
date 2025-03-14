d2c985194cd082bc251680844c130b8c9b3ba31d
Admillz712 <admillz@udel.edu>
2025-03-14 14:55:07 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App(): React.JSX.Element {
             </p>
 =======
             <hr></hr>
-            {/* <DoubleHalf></DoubleHalf> */}
+            {<DoubleHalf></DoubleHalf>}
             <hr></hr>
             <ChooseTeam></ChooseTeam>
             <hr></hr>
